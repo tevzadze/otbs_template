df300d8ce80a604bb64f8c92a09e2c254ccbd488
Davyd Tevzadze <pawtod20@gmail.com>
2020-10-30 22:01:07 +0200

    jQuery renovation

--- gulpfile.js
+++ gulpfile.js
@@ -45,7 +45,7 @@ gulp.task('styles', function() {
 // JS
 gulp.task('scripts', function() {
 	return gulp.src([
-		'app/libs/jquery/dist/jquery.min.js',
+		'app/libs/jquery/dist/jquery-3.5.1.min.js',
 		'app/js/common.js', // Always at the end
 		], { allowEmpty: true })
 	.pipe(concat('scripts.min.js'))
